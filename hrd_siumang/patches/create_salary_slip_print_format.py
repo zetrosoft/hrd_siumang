3de890164445ecb559efc24a3ccc91f58467eaf0
Bijak Technology <iswanputera@gmail.com>
2025-11-28 21:52:49 +0700

    fix print format

--- hrd_siumang/patches/create_salary_slip_print_format.py
+++ hrd_siumang/patches/create_salary_slip_print_format.py
@@ -287,7 +287,7 @@ def execute():
 	print_format_data = {
 		"name": "Slip Gaji Siumang",
 		"doc_type": "Salary Slip",
-		"module": "hrd_siumangg",  # Corrected capitalization based on app_title
+		"module": "hrd_siumang",  # Corrected capitalization based on app_title
 		"standard": "Yes",
 		"print_format_type": "Jinja",
 		"html": html_content,
